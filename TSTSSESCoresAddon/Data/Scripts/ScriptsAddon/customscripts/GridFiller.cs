5dd6aa96ba05e8ffc27472e48cf0397d4decef6a
InvalidArgument3 <InvalidArgument3@gmail.com>
2023-12-17 13:25:04 -0600

    Update GridFiller.cs

--- TSTSSESCoresAddon/Data/Scripts/ScriptsAddon/customscripts/GridFiller.cs
+++ TSTSSESCoresAddon/Data/Scripts/ScriptsAddon/customscripts/GridFiller.cs
@@ -4,9 +4,6 @@ using Sandbox.ModAPI;
 using Scripts.Specials.ShipClass;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VRage;
 using VRage.Game;
 using VRage.Game.Components;
@@ -20,6 +17,7 @@ namespace TSTSSESCoresAddon.Data.Scripts.ScriptsAddon.customscripts
     public class GridFiller : MySessionComponentBase
     {
         private static bool isServer;
+        private static Dictionary<long, HashSet<long>> blockGroups = new Dictionary<long, HashSet<long>>();
 
         static GridFiller()
         {
@@ -40,16 +38,21 @@ namespace TSTSSESCoresAddon.Data.Scripts.ScriptsAddon.customscripts
             }
         }
 
-        //This takes effect works after the grid is cut/pasted or spec block is deleted
         private static void OnSpecBlockCreated(object specBlock)
         {
             var tBlock = SpecBlockHooks.GetBlockSpecCore(specBlock);
             if (tBlock == null)
                 return;
+
             MyAPIGateway.Utilities.ShowNotification($"SpecBlock {tBlock.DisplayNameText} placed!");
 
-            AddBlock<MyObjectBuilder_Reactor>(tBlock, "LargeBlockSmallGenerator", tBlock.Position + (Vector3I)tBlock.LocalMatrix.Forward);
-            AddBlock<MyObjectBuilder_CargoContainer>(tBlock, "LargeBlockSmallContainer", tBlock.Position + (Vector3I)tBlock.LocalMatrix.Backward);
+            HashSet<long> associatedBlocks = new HashSet<long>();
+            associatedBlocks.Add(AddBlock<MyObjectBuilder_Reactor>(tBlock, "LargeBlockSmallGenerator", tBlock.Position + (Vector3I)tBlock.LocalMatrix.Forward));
+            associatedBlocks.Add(AddBlock<MyObjectBuilder_CargoContainer>(tBlock, "LargeBlockSmallContainer", tBlock.Position + (Vector3I)tBlock.LocalMatrix.Backward));
+
+            blockGroups.Add(tBlock.EntityId, associatedBlocks);
+
+            tBlock.CubeGrid.OnBlockRemoved += Grid_OnBlockRemoved;
         }
 
         private static void OnSpecBlockDestroyed(object specBlock)
@@ -57,13 +60,16 @@ namespace TSTSSESCoresAddon.Data.Scripts.ScriptsAddon.customscripts
             var tBlock = SpecBlockHooks.GetBlockSpecCore(specBlock);
             if (tBlock == null)
                 return;
+
             MyAPIGateway.Utilities.ShowNotification($"SpecBlock {tBlock.DisplayNameText} removed!");
+            blockGroups.Remove(tBlock.EntityId);
+
+            tBlock.CubeGrid.OnBlockRemoved -= Grid_OnBlockRemoved;
         }
 
-        private static void AddBlock<T>(IMyTerminalBlock block, string subtypeName, Vector3I position) where T : MyObjectBuilder_CubeBlock, new()
+        private static long AddBlock<T>(IMyTerminalBlock block, string subtypeName, Vector3I position) where T : MyObjectBuilder_CubeBlock, new()
         {
             var grid = block.CubeGrid;
-
             var nextBlockBuilder = new T
             {
                 SubtypeName = subtypeName,
@@ -80,9 +86,23 @@ namespace TSTSSESCoresAddon.Data.Scripts.ScriptsAddon.customscripts
             if (newBlock == null)
             {
                 MyAPIGateway.Utilities.ShowNotification($"Failed to add {subtypeName}", 1000);
-                return;
+                return 0;
             }
             MyAPIGateway.Utilities.ShowNotification($"{subtypeName} added at {position}", 1000);
+            return newBlock.FatBlock.EntityId;
+        }
+
+        private static void Grid_OnBlockRemoved(IMySlimBlock block)
+        {
+            long removedBlockId = block.FatBlock?.EntityId ?? 0;
+            foreach (var group in blockGroups)
+            {
+                if (group.Value.Contains(removedBlockId))
+                {
+                    MyAPIGateway.Utilities.ShowNotification("A part of a SpecBlock assembly has been removed!", 5000);
+                    break;
+                }
+            }
         }
     }
 }
