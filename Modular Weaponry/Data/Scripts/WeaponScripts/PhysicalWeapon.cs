291c012505269fcd0932533e229aca8f3d7d8b00
Aristeas <94058548+Jnick-24@users.noreply.github.com>
2023-12-21 21:52:07 -0600

    kms

---
+++ Modular Weaponry/Data/Scripts/WeaponScripts/PhysicalWeapon.cs
@@ -0,0 +1,91 @@
+﻿using Sandbox.Game.Entities.Cube;
+using Sandbox.ModAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.Utils;
+
+namespace Modular_Weaponry.Data.Scripts.WeaponScripts
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation | MyUpdateOrder.AfterSimulation)]
+    public class PhysicalWeapon : MySessionComponentBase
+    {
+        public WeaponPart basePart;
+        private List<WeaponPart> componentParts = new List<WeaponPart>();
+
+        public int numReactors = 0;
+
+        public PhysicalWeapon(WeaponPart basePart)
+        {
+            this.basePart = basePart;
+            RecursiveWeaponChecker(basePart);
+            componentParts.Add(basePart);
+
+            MyAPIGateway.Utilities.ShowNotification("Weapon parts: " + componentParts.Count);
+        }
+
+        public void AddPart(WeaponPart part)
+        {
+            componentParts.Add(part);
+            part.memberWeapon = this;
+
+            // TODO remove. Test for counting number of reactors; i.e. PAC cannon
+            numReactors = 0;
+            foreach (var cPart in componentParts)
+            {
+                if (cPart.block.BlockDefinition.Id.SubtypeName == "LargeBlockSmallGenerator")
+                {
+                    List<IMySlimBlock> blocks2 = new List<IMySlimBlock>();
+                    cPart.block.GetNeighbours(blocks2);
+
+                    if (blocks2.Count >= 2)
+                        numReactors++;
+                }
+            }
+            WeaponDefiniton.numReactors = numReactors;
+            MyAPIGateway.Utilities.ShowNotification("Reactors: " + numReactors);
+
+            MyAPIGateway.Utilities.ShowNotification("Weapon parts: " + componentParts.Count);
+        }
+
+        public void Remove(WeaponPart part)
+        {
+            if (!componentParts.Contains(part))
+                return;
+            componentParts.Remove(part);
+            MyAPIGateway.Utilities.ShowNotification("Weapon parts: " + componentParts.Count);
+        }
+
+        private void RecursiveWeaponChecker(WeaponPart currentBlock)
+        {
+            componentParts.Clear();
+
+            List<IMySlimBlock> slimNeighbors = new List<IMySlimBlock>();
+            currentBlock.block.GetNeighbours(slimNeighbors);
+
+            foreach (IMySlimBlock neighbor in slimNeighbors)
+            {
+                if (WeaponDefiniton.IsBlockAllowed(neighbor))
+                {
+                    WeaponPart neighborPart;
+                    
+                    if (WeaponPartGetter.AllWeaponParts.TryGetValue(neighbor, out neighborPart))
+                    {
+                        // Avoid double-including blocks
+                        if (componentParts.Contains(neighborPart))
+                            continue;
+
+                        MyLog.Default.WriteLineAndConsole("Add part " + neighbor.BlockDefinition.Id.SubtypeName + " @ " + neighbor.Position);
+
+                        componentParts.Add(neighborPart);
+                        RecursiveWeaponChecker(neighborPart);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Modular Weaponry/Data/Scripts/WeaponScripts/WeaponPartGetter.cs
@@ -0,0 +1,119 @@
+﻿using CoreSystems.Api;
+using Sandbox.ModAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRage.Game.Components;
+using VRage.Game.Entity;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRageMath;
+
+namespace Modular_Weaponry.Data.Scripts.WeaponScripts
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
+    public class WeaponPartGetter : MySessionComponentBase
+    {
+        public static WeaponPartGetter Instance; // the only way to access session comp from other classes and the only accepted static field.
+        public static Dictionary<IMySlimBlock, WeaponPart> AllWeaponParts = new Dictionary<IMySlimBlock, WeaponPart>();
+
+        private List<IMySlimBlock> queuedBlockAdds = new List<IMySlimBlock>();
+
+        public static WcApi wAPI;
+
+        public override void LoadData()
+        {
+            wAPI = new WcApi();
+            Instance = this;
+            MyAPIGateway.Entities.OnEntityAdd += OnGridAdd;
+            wAPI.Load();
+        }
+
+        protected override void UnloadData()
+        {
+            AllWeaponParts = null;
+            wAPI = null;
+
+            Instance = null; // important for avoiding this object to remain allocated in memory
+        }
+
+        public override void UpdateAfterSimulation()
+        {
+            base.UpdateAfterSimulation();
+
+            // Queue gridadds to account for world load/grid pasting
+            foreach (var queuedBlock in queuedBlockAdds)
+            {
+                OnBlockAdd(queuedBlock);
+            }
+            queuedBlockAdds.Clear();
+        }
+
+        private void OnGridAdd(IMyEntity entity)
+        {
+            if (!(entity is IMyCubeGrid))
+                return;
+
+            IMyCubeGrid grid = (IMyCubeGrid) entity;
+
+            // Exclude projected and held grids
+            if (grid.Physics == null)
+                return;
+
+            grid.OnBlockAdded += OnBlockAdd;
+            grid.OnBlockRemoved += OnBlockRemove;
+
+            List<IMySlimBlock> existingBlocks = new List<IMySlimBlock>();
+            grid.GetBlocks(existingBlocks);
+            foreach (var block in existingBlocks)
+                queuedBlockAdds.Add(block);
+        }
+
+        private void OnBlockAdd(IMySlimBlock block)
+        {
+            if (wAPI.IsReady && block.FatBlock != null)
+            {
+                try
+                {
+                    if (wAPI.HasCoreWeapon((MyEntity)block.FatBlock))
+                    {
+                        wAPI.AddProjectileCallback((MyEntity)block.FatBlock, 0, ProjectileCallback);
+                    }
+                }
+                catch
+                {
+                    MyAPIGateway.Utilities.ShowNotification("it threw an error dumbass");
+                }
+            }
+
+            if (!WeaponDefiniton.IsBlockAllowed(block))
+                return;
+
+            WeaponPart w = new WeaponPart(block);
+        }
+        private void ProjectileCallback(long firerEntityId, int firerPartId, ulong projectileId, long targetEntityId, Vector3D projectilePosition, bool projectileExists)
+        {
+            if (projectileExists)
+                wAPI.SetProjectileState(projectileId, WeaponDefiniton.ChangeProjectileData(firerEntityId, firerPartId, projectileId, targetEntityId, projectilePosition));
+        }
+
+
+
+
+
+
+
+        private void OnBlockRemove(IMySlimBlock block)
+        {
+            WeaponPart part;
+            if (AllWeaponParts.TryGetValue(block, out part))
+            {
+                //MyAPIGateway.Utilities.ShowNotification("Removing");
+                part.memberWeapon?.Remove(part);
+                AllWeaponParts.Remove(block);
+            }
+        }
+    }
+}
