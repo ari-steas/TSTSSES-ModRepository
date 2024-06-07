ecf18aa33870223ad7b05daa6e2d94650bbc1acc
Aristeas <94058548+Jnick-24@users.noreply.github.com>
2024-06-06 20:58:23 -0500

    spawn asteroid

---
+++ Dynamic Asteroids/Data/Scripts/DynamicAsteroids/AsteroidEntities/AsteroidSpawner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Sandbox.ModAPI;
+using VRageMath;
+
+namespace DynamicAsteroids.AsteroidEntities
+{
+    internal class AsteroidSpawner
+    {
+        private List<AsteroidEntity> _asteroids;
+
+        public void Init()
+        {
+            _asteroids = new List<AsteroidEntity>(AsteroidSettings.MaxAsteroidCount);
+        }
+
+        public void Close()
+        {
+            _asteroids.Clear();
+        }
+
+        public void UpdateTick()
+        {
+            Vector3D playerPosition = MyAPIGateway.Session?.Player?.GetPosition() ?? Vector3D.MaxValue;
+
+            if (playerPosition == Vector3D.MaxValue || !AsteroidSettings.SpawnAsteroidsAtPoint(playerPosition))
+                return;
+
+            foreach (var asteroid in _asteroids.ToArray())
+            {
+                if (Vector3D.DistanceSquared(asteroid.PositionComp.GetPosition(), playerPosition) >
+                    AsteroidSettings.AsteroidSpawnRadius * AsteroidSettings.AsteroidSpawnRadius * 1.1)
+                {
+                    _asteroids.Remove(asteroid);
+                    continue;
+                }
+            }
+
+            int asteroidsSpawned = 0;
+            while (_asteroids.Count < AsteroidSettings.MaxAsteroidCount && asteroidsSpawned < 10)
+            {
+                _asteroids.Add(AsteroidEntity.CreateAsteroid(playerPosition + RandVector()*AsteroidSettings.AsteroidSpawnRadius, RandAsteroidSize, Vector3D.Zero));
+
+                asteroidsSpawned++;
+            }
+        }
+
+        private Vector3D RandVector()
+        {
+            var theta = MainSession.I.Rand.NextDouble() * 2.0 * Math.PI;
+            var phi = Math.Acos(2.0 * MainSession.I.Rand.NextDouble() - 1.0);
+            var sinPhi = Math.Sin(phi);
+            return Math.Pow(MainSession.I.Rand.NextDouble(), 1/3d) * new Vector3D(sinPhi * Math.Cos(theta), sinPhi * Math.Sin(theta), Math.Cos(phi));
+        }
+
+        private float RandAsteroidSize => (float) (MainSession.I.Rand.NextDouble()*MainSession.I.Rand.NextDouble()*MainSession.I.Rand.NextDouble()*500) + 1.5f;
+    }
+}
