f799708d97d86c670a777a6be7519591ee59c3a2
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-06-25 20:56:19 -0500

    add character collision damage disabler

---
+++ CharacterDamageDisabler/Data/Scripts/CharDamageDisabler/CharacterDamageDisabler.cs
@@ -0,0 +1,42 @@
+﻿using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game.Entities;
+using Sandbox.ModAPI;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.Utils;
+
+namespace DisablePlayerCollisionDamageMod
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class DisablePlayerCollisionDamage : MySessionComponentBase
+    {
+        public override void BeforeStart()
+        {
+            // Register the damage handler
+            MyAPIGateway.Session.DamageSystem.RegisterBeforeDamageHandler(0, BeforeDamageApplied);
+        }
+
+        protected override void UnloadData()
+        {
+            // No need to unregister as there's no unregister method
+        }
+
+        private void BeforeDamageApplied(object target, ref MyDamageInformation info)
+        {
+            // Check if the target is a player character
+            if (target is IMyCharacter)
+            {
+                // Show the damage type in a notification
+                //MyAPIGateway.Utilities.ShowNotification($"DamageType: {info.Type}", 2000, MyFontEnum.Green);
+
+                // Check if the damage type is collision
+                if (info.Type == MyDamageType.Environment || info.Type == MyDamageType.Fall)
+                {
+                    // Set damage amount to zero for collision damage
+                    info.Amount = 0f;
+                }
+            }
+        }
+    }
+}
